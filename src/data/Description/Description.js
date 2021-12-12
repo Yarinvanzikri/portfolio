5b62793085ac1664e9831b55a919b44fcdc318a7
Yarin Van Zikri <yarinvanzikri@gmail.com>
2021-12-12 18:38:28 +0200

    initial deployment

--- src/data/Description/Description.js
+++ src/data/Description/Description.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from "framer-motion"
 import './Description.scss';
+import Logo from "../../design/Logo/Logo";
 function Description(props) {
     return (
         <motion.div className='Description'
@@ -9,12 +10,9 @@ function Description(props) {
           transition={{duration: 1.5, type: "spring" , delay: 3.5}}
         >
             <div className="about-me">
-                <h1>Hi My name Is Yarin</h1>
-                {/*<p> Junior Node.js developer,<br /> with experience in Front-end technologies such as HTML,*/}
-                {/*    CSS, React.js as well as MongoDB, REST API, and UI/UX.<br /> Graduated from Netcraft*/}
-                {/*    Fullstack course. Looking for entry-level roles in tech companies with*/}
-                {/*    opportunities to learn and grow.</p>*/}
+                <Logo/>
             </div>
+
 
         </motion.div>
     );
---
+++ src/design/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../assets/screen2.png'
+import './Logo.scss';
+
+function Logo(props) {
+    return (
+        <div>
+            <div className={'logo'}>
+                <img src={logo} />
+            </div>
+            <div className={'mainHeader'}>
+                <h1>Yarin Ben - Zikri</h1>
+            </div>
+            <div className={'secondaryHeader'}>
+                <h3>Full - Stack Developer</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;
